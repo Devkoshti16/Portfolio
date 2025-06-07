5ae74ec6bafef129c08aa9d7f78b135d1b93b86e
Dk world <dkworld177@gmail.com>
2025-06-07 09:48:43 +0530

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,15 @@ import "./Home.css";
 import heroImage from "../../assets/Hero.png"; // Ensure correct path
 
 function Home() {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = cvFile;
+    link.download = "cv.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  
   return (
     <>
       <section className="hero">
@@ -18,6 +27,31 @@ function Home() {
             <h2>2</h2>
             <p>YEARS EXPERIENCE</p>
           </div>
+          <a onClick={handleDownload} className="button">
+            <div className="button-wrapper">
+              <div className="text">Download CV</div>
+              <span className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="img"
+                  width="2em"
+                  height="2em"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17"
+                  ></path>
+                </svg>
+              </span>
+            </div>
+          </a>
           <div className="social-icons">
             <i className="fab fa-facebook"></i>
             <i className="fab fa-twitter"></i>
@@ -26,9 +60,9 @@ function Home() {
         </div>
 
         <div className="hero-image">
-          <img src={heroImage} alt="John Deo" /> 
+          <img src={heroImage} alt="John Deo" />
         </div>
-        
+
         <div className="reviews">
           <p>12k Reviews On</p>
           <div className="stars">⭐⭐⭐⭐⭐</div>
