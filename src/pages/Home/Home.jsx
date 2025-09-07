40a9cd02253830946cdeed6996ba27f360e05809
Dk world <dkworld177@gmail.com>
2025-09-07 11:09:11 +0530

    home image code update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,6 +14,7 @@ function Home() {
   };
   
   return (
+    // Hero Section Start Here //
     <>
       <section className="hero">
         <div className="hero-content">
@@ -32,23 +33,11 @@ function Home() {
             <div className="button-wrapper">
               <div className="text">Download CV</div>
               <span className="icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                  role="img"
-                  width="2em"
-                  height="2em"
-                  preserveAspectRatio="xMidYMid meet"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17"
-                  ></path>
+                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img"
+                  width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                  <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth="2" d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17">
+                  </path>
                 </svg>
               </span>
             </div>
@@ -66,6 +55,7 @@ function Home() {
         </div>
       </section>
     </>
+  // Hero Section End Here //
   );
 }
 
